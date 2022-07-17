af4f7ccecde2690af27c2f54112d88f8a39a4c9f
Hafsa Moontari Ali <hafsa390@gmail.com>
2022-07-17 02:12:23 -0400

    update test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -9,6 +9,10 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 // import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
+it('correct addition', () => {
   expect(1 + 1).toBe(2)
+});
+
+it('incorrect addition', () => {
+  expect(1 + 1).toBe(3)
 });
